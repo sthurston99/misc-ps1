97963624dd9dd12b142d5e5f0ff2ce8110dc2700
Simon Thurston <simongethurston@gmail.com>
2023-05-10 11:01:10 -0400

    switched from spaces to tabs

--- fixMappedDrives.ps1
+++ fixMappedDrives.ps1
@@ -7,8 +7,8 @@ $pw = Read-Host "Enter your password" -AsSecureString
 $cred = New-Object System.Management.Automation.PSCredential ("$dom\$user", $pw)
 
 Get-WmiObject -Class Win32_MappedLogicalDisk | ForEach-Object {
-    net use $_.Name /delete
-    $provider = $_.ProviderName
-    Start-Sleep 3
-    New-PSDrive -Name $_.Name.SubString(0, 1) -PSProvider FileSystem -Credential $cred -Root $provider -Persist -Scope Global
+	net use $_.Name /delete
+	$provider = $_.ProviderName
+	Start-Sleep 3
+	New-PSDrive -Name $_.Name.SubString(0, 1) -PSProvider FileSystem -Credential $cred -Root $provider -Persist -Scope Global
 }
